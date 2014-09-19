62931b14b6ccaaa6910b3a0f56a9c5b4b0096ce9
Kyle Goines <thirteenotherslikeme@gmail.com>
2014-09-19 12:24:48 -0400

    portfolio :)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,50 @@
+/*
+$('header .contact h3').on('click',function(){
+	if($( window ).width() < 750){
+		$('html, body').animate({
+        	scrollTop: $("footer .contact").offset().top,
+    	},
+ 		{
+        	duration: 500,
+        	complete: function () {
+        		alert('aaaa');
+        	}
+    	}
+    })
+})
 
+*/
+
+
+$('header .contact h3').on('click',function(){
+
+
+	if($( window ).width() < 750){
+
+
+		$('html, body').animate(
+
+
+		{
+        	scrollTop: $("footer .contact").offset().top,
+    	},
+    	{
+    		duration: 1000,
+    		complete: function(){
+    			$('footer .contact h3').addClass('alertMe')
+    			$('footer .contact a').addClass('alertMe')
+    		}
+    	}
+
+
+
+
+			)
+
+
+
+
+
+	}
+
+})
